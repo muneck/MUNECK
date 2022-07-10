4cc7791e481955969faffd230d54629435e45043
KevDevPython <2357931342@qq.com>
2022-07-10 21:45:42 +0800

    add: tracking top score

--- Plugins/BlueprintJson/Source/BlueprintJson/Private/BlueprintJsonModule.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 Maksim Shestakov. All Rights Reserved.
-
-#include "CoreMinimal.h"
-#include "Modules/ModuleInterface.h"
-#include "Modules/ModuleManager.h"
-
-
-IMPLEMENT_MODULE(IModuleInterface, BlueprintJson)
